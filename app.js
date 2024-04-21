ec7b8a4874c9d25c162f9182e8cfd8742980f8f5
J Kimball <jeffkimballis@gmail.com>
2024-04-21 17:16:57 -0500

    add test routes and add body parser package

--- app.js
+++ app.js
@@ -4,14 +4,31 @@ const express = require('express')
 
 const app = express()
 
+// app.use
+
 
 
 app.get('/favicon.ico', (req, res) => res.status(204).end())
 
+app.use('/',(req, res, next) => {
+    console.log('This always runs')
+    next()
+})
+
+app.use('/add-product', (req, res, next) => {
+    console.log('Add Product')
+    res.send('<form action="/product" method=""POST><label>Enter a Product </label><input name="productTitle"><button type="submit">Submit</button></form>')
+
+})
+
+app.use('/product', (req, res, next) => {
+    
+    res.redirect('/')
+})
 
 app.use('/', (req, res, next) => {
-    console.log('In another middleware!')
-    res.send('<h1>Hello from Express!</h1>')
+    console.log('Show Hello Message!')
+    res.send('<h1>Hello from Express!</h1><small> URL route =  "/" or nothing</small>')
 })
 
 app.listen(3000)
